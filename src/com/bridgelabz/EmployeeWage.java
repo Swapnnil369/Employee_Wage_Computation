45fa099e5afcc691b222f76abeae31c54ba010e8
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-12 23:56:27 +0530

    Added :Calculate Monthly wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,33 +1,37 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
-    static final int FULL_TIME_Work = 1;
-    static final int PART_TIME_Work = 2;
+    public static final int PART_TIME_WORK = 2;
+    public static final int FULL_TIME_WORK = 1;
+    public static final int EMP_RATE_PER_HOUR = 20;
+    public static final int NUM_OF_WORKING_DAY = 20;
 
     public static void main(String[] args) {
+        int empHour = 0, empWage = 0, totalWage = 0;
+        int day=0;
+        for (int i = 0; i < NUM_OF_WORKING_DAY; i++) {
+            int empCheck = (int) Math.floor(Math.random() * 10) % 3;
 
-        int EMP_RATE_PER_HOUR = 20;
-        int empHrs = 0;
-        int empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        switch (empCheck) {
+            switch (empCheck) {
+                case FULL_TIME_WORK:
+                    empHour = 8;
+                    break;
 
-            case FULL_TIME_Work:
-                System.out.println("Full time work");
-                empHrs = 8;
-                break;
-            case PART_TIME_Work:
-                System.out.println("Part time work");
-                empHrs = 4;
-                break;
-            default:
-                empHrs = 0;
+                case PART_TIME_WORK:
+                    empHour = 4;
+                    break;
+
+                default:
+                    empHour = 0;
+            }
+
+            empWage = empHour * EMP_RATE_PER_HOUR;
+            totalWage += empWage;
+            day++;
+            System.out.println("Employee wages are : " + empWage);
         }
 
-        empWage = empHrs * EMP_RATE_PER_HOUR;
-        System.out.println("The total employeeWage are : " + empWage);
-
+        System.out.println("Day :"+day+" total employee wages are : " + totalWage);
     }
-
 
 }
