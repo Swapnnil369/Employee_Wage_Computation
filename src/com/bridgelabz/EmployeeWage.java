cd75d6b7843737fc8ac71959c273e87e1fae0303
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-12 23:41:07 +0530

    Added :Calculate wage using switch case

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -1,19 +1,27 @@
 package com.bridgelabz;
 
 public class EmployeeWage {
+    static final int FULL_TIME_Work = 1;
+    static final int PART_TIME_Work = 2;
+
     public static void main(String[] args) {
-        int FULL_TIME_Work = 1;
-        int PART_TIME_Work = 2;
+
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
         int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-        if (empCheck == FULL_TIME_Work) {
-            empHrs = 8;
-        } else if (empCheck == PART_TIME_Work) {
-            empHrs = 4;
-        } else {
-            empHrs = 0;
+        switch (empCheck) {
+
+            case FULL_TIME_Work:
+                System.out.println("Full time work");
+                empHrs = 8;
+                break;
+            case PART_TIME_Work:
+                System.out.println("Part time work");
+                empHrs = 4;
+                break;
+            default:
+                empHrs = 0;
         }
 
         empWage = empHrs * EMP_RATE_PER_HOUR;
