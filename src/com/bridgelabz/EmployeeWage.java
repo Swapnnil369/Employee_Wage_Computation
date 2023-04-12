4fd710a08461da85c61bbff5d9660307cd73016e
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-12 23:28:35 +0530

    Added :Calculate part time wage

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,14 +3,16 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static void main(String[] args) {
         int FULL_TIME_Work = 1;
+        int PART_TIME_Work = 2;
         int EMP_RATE_PER_HOUR = 20;
         int empHrs = 0;
         int empWage = 0;
-        int empCheck = (int) Math.floor(Math.random() * 10) % 2;
+        int empCheck = (int) Math.floor(Math.random() * 10) % 3;
         if (empCheck == FULL_TIME_Work) {
             empHrs = 8;
-        }
-        else {
+        } else if (empCheck == PART_TIME_Work) {
+            empHrs = 4;
+        } else {
             empHrs = 0;
         }
 
