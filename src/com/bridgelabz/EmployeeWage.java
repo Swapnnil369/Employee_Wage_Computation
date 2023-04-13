7b6589e5ce822aeab90c205efd1e5198588a2b84
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-13 08:51:14 +0530

    Added :Save total wage for each company

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,7 +3,19 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static final int PART_TIME_Work = 1;
     public static final int FUll_TIME_WORK = 2;
-    public static int computeEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+    public String company;
+    public int empRatePerHour;
+    public int numOfWorkingDays;
+    public int maxHoursPerMonth;
+    int totalEmpWage = 0;
+
+    public EmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        this.company = company;
+        this.empRatePerHour = empRatePerHour;
+        this.numOfWorkingDays = numOfWorkingDays;
+        this.maxHoursPerMonth = maxHoursPerMonth;
+    }
+    public void computeEmployeeWage() {
         int totalEmpHrs = 0, totalWorkingDays = 0;
 
         while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
@@ -28,17 +40,31 @@ public class EmployeeWage {
             }
 
             totalEmpHrs += empHrs;
-            System.out.println("Day " +totalWorkingDays+  " :"  + " " + "Emp Hrs : " + empHrs);
+            System.out.println("Day " + totalWorkingDays + " :" + " " + "Emp Hrs : " + empHrs);
         }
-        int totalEmpWage = totalEmpHrs * empRatePerHour;
-        System.out.println("Total Employee wage for company : " + company + " is :" + totalEmpWage);
-        return totalEmpWage;
-
+        totalEmpWage = totalEmpHrs * empRatePerHour;
     }
 
+    @Override
+    public String toString() {
+        return "EmployeeWage{" +
+                "company='" + company + '\'' +
+                ", empRatePerHour=" + empRatePerHour +
+                ", numOfWorkingDays=" + numOfWorkingDays +
+                ", maxHoursPerMonth=" + maxHoursPerMonth +
+                ",Total employee wage for company : " + company + " " + "is:" + totalEmpWage +
+                '}';
+    }
     public static void main(String[] args) {
 
-        computeEmployeeWage("TCS", 20, 2, 10);
-        computeEmployeeWage("INFOSYS", 10, 3, 20);
+        EmployeeWage tcs = new EmployeeWage("TCS", 20, 2, 10);
+        EmployeeWage infosys = new EmployeeWage("INFOSYS", 20, 2, 10);
+
+        tcs.computeEmployeeWage();
+        System.out.println(tcs);
+
+        infosys.computeEmployeeWage();
+        System.out.println(infosys);
+
     }
 }
