b1e2f71940b3542cc2e79cc87a73b2fde215172f
Swapnil639 <swapnilpatil631997@gmail.com>
2023-04-13 08:43:15 +0530

    Added :Calculate Employee wage for multiple companies

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -3,14 +3,12 @@ package com.bridgelabz;
 public class EmployeeWage {
     public static final int PART_TIME_Work = 1;
     public static final int FUll_TIME_WORK = 2;
-    public static final int EMP_RATE_PER_HOUR = 20;
-    public static final int NUM_OF_WORKING_DAYS = 30;
-    public static final int MAX_HRS_IN_MONTH = 100;
+    public static int computeEmployeeWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth) {
+        int totalEmpHrs = 0, totalWorkingDays = 0;
 
-    public static void computeEmployeeWage() {
-        int totalEmpHrs = 0, totalEmpWage = 0, totalWorkingDays = 0;
-        int empHrs = 0;
-        while (totalEmpHrs < MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays) {
+
+            int empHrs = 0;
             totalWorkingDays++;
 
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
@@ -25,22 +23,22 @@ public class EmployeeWage {
                     empHrs = 4;
                     break;
 
-                default :
+                default:
                     empHrs = 0;
             }
 
             totalEmpHrs += empHrs;
-            System.out.println("Working hours : " +empHrs);
-            int empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            System.out.println("Employee Wage : " +empWage);
+            System.out.println("Day " +totalWorkingDays+  " :"  + " " + "Emp Hrs : " + empHrs);
         }
-        System.out.println("Total working hours : " +totalEmpHrs);
-        System.out.println("Total employee wage : " +totalEmpWage);
+        int totalEmpWage = totalEmpHrs * empRatePerHour;
+        System.out.println("Total Employee wage for company : " + company + " is :" + totalEmpWage);
+        return totalEmpWage;
+
     }
 
     public static void main(String[] args) {
 
-        computeEmployeeWage();
+        computeEmployeeWage("TCS", 20, 2, 10);
+        computeEmployeeWage("INFOSYS", 10, 3, 20);
     }
 }
